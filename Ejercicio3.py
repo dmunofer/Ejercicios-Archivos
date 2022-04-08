1e072a2b8ce58d65d037cb8bbd9d445a1e560d54
dmunofer <diego.fmz@hotmail.com>
2022-04-08 17:29:27 +0200

    ej3 terminado

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -6,4 +6,8 @@ def aprobados_suspensos(lista):
     for dicc in lista:
         list(dicc)
         if dicc[2]>=75 and ((dicc[3],dicc[4],dicc[7],dicc[8])>=(4,4,4,4)) and dicc[-1]>=5:
-            
+            aprobados.append(dict(dicc))
+        else:
+            suspensos.append(dict(dicc))
+    return lista
+
