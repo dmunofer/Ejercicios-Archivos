cde3ac53253722d25d51b899b034bf33454731b8
dmunofer <diego.fmz@hotmail.com>
2022-04-08 17:11:04 +0200

    ej 1 sin pandas

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -3,3 +3,13 @@ import pandas as pd
 def diccionario_csv(filename):
     dicc= pd.read_csv(filename).to_dict()
     return dicc
+
+
+def sin_pandas(filename):
+    lista=[]
+    archivo = (filename, "r")
+    for linea in archivo:
+        lista.append(dict(linea))
+
+    archivo.close()
+    return lista
